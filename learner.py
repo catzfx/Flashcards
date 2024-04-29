6ce545b3f70d9dc3ca384f2cc9169f978df21f77
catzfx <iustinandi@gmail.com>
2024-04-29 19:28:06 +0300

    make learner.py modular and add main loop

--- learner.py
+++ learner.py
@@ -5,7 +5,12 @@ from extra import wipe
 db = TinyDB('./db.json')
 settings = TinyDB('./settings.json')
 ease_list = settings.get(doc_id=1).get('ease')
-due_count = 0
+
+def run():
+    try:
+        learner()
+    except KeyboardInterrupt():
+        pass
 
 def due_search():
     due_list = []
@@ -14,45 +19,48 @@ def due_search():
         if delta.days >= 0:
             due_list.append((card, card.doc_id))
     return due_list
-        
-    
-wipe()
-due_list = due_search()
-print(f'{len(due_list)} cards due')
-input('Press -enter- to enter learning mode\n(you can press ctrl-c at any time to quit)\n')
 
-while len(due_list) > 0:
-    for due_card, due_id in due_list:
-        wipe()
-        print(f'ID: {due_id}', end= "  ")
-        print(f'Category: {due_card.get("category")}')
-        print(f'Q: {due_card.get("q")}')
-        input('\nPress -enter- to show answer\n')
-        wipe()
-        print(f'ID: {due_id}', end= "  ")
-        print(f'Category: {due_card.get("category")}')
-        print(f'Q: {due_card.get("q")}')
-        print(f'A: {due_card.get("a")}')
-        print('\nf. I knew it  ----   j. Again')
-        option = input("> ")
-        if option == 'f':
-            card_ease = due_card.get('count')
-            time_change = datetime.timedelta(minutes = 2)
-            if card_ease < 5:
-                time_change = datetime.timedelta(minutes=ease_list[card_ease])
-
-            else:
-                time_change = datetime.timedelta(minutes=1440 * pow(2, card_ease - 4))
-
-            db.update({'due': str(datetime.datetime.now() + time_change),
-                       'count': card_ease + 1}, doc_ids=[due_id])
-        elif option == 'j':
-            card_ease = due_card.get('count')
-            if card_ease > 5:
-                card_ease = 3
-            else:
-                card_ease = 0
-            time_change = datetime.timedelta(minutes = ease_list[card_ease])
-            db.update({'count': card_ease,
-                       'due': str(datetime.datetime.now() + time_change)},doc_ids=[due_id])
+def learner():
+    wipe()
     due_list = due_search()
+    print(f'{len(due_list)} cards due')
+    input('Press -enter- to enter learning mode\n(you can press ctrl-c at any time to quit)\n')
+
+    while len(due_list) > 0:
+        for due_card, due_id in due_list:
+            wipe()
+            print(f'ID: {due_id}', end= "  ")
+            print(f'Category: {due_card.get("category")}')
+            print(f'Q: {due_card.get("q")}')
+            input('\nPress -enter- to show answer\n')
+            wipe()
+            print(f'ID: {due_id}', end= "  ")
+            print(f'Category: {due_card.get("category")}')
+            print(f'Q: {due_card.get("q")}')
+            print(f'A: {due_card.get("a")}')
+            print('\nf. I knew it  ----   j. Again')
+            option = input("> ")
+            if option == 'f':
+                card_ease = due_card.get('count')
+                time_change = datetime.timedelta(minutes = 2)
+                if card_ease < 5:
+                    time_change = datetime.timedelta(minutes=ease_list[card_ease])
+
+                else:
+                    time_change = datetime.timedelta(minutes=1440 * pow(2, card_ease - 4))
+
+                db.update({'due': str(datetime.datetime.now() + time_change),
+                        'count': card_ease + 1}, doc_ids=[due_id])
+            elif option == 'j':
+                card_ease = due_card.get('count')
+                if card_ease > 5:
+                    card_ease = 3
+                else:
+                    card_ease = 0
+                time_change = datetime.timedelta(minutes = ease_list[card_ease])
+                db.update({'count': card_ease,
+                        'due': str(datetime.datetime.now() + time_change)},doc_ids=[due_id])
+        due_list = due_search()
+    if len(due_list) == 0:
+        wipe()
+        input("No more cards due. Press enter to return to main menu")
