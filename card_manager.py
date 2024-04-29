cf68ebdc1ebc6a9e89874d0179db2d68844907b6
catzfx <iustinandi@gmail.com>
2024-04-29 19:43:45 +0300

    modularize card manager

--- card_manager.py
+++ card_manager.py
@@ -3,36 +3,42 @@ from datetime import datetime
 from extra import wipe
 db = TinyDB('./db.json')
 cardq = Query()
-print('\n\n::Card Manager::')
 
-while True:
-    wipe()
-    print('Card Manager\n\n1. list all\n2. add card\n3. remove card\n4. Quit')
-    print('> ', end='')
-    option = int(input())
-    if option == 4:
-        wipe(quit=True)
-        break
-    elif option == 3:
-        card_id = int(input('card id to delete: '))
-        db.remove(doc_ids=[card_id])
-    elif option == 2:
-        card_category = input('card category: ')
-        card_qtion = input('card question: ')
-        card_answ = input('card asnwer: ')
-        db.insert({'category': card_category,
-                   'q': card_qtion,
-                   'a': card_answ,
-                   'count': 0,
-                   'due': str(datetime.now())
-                   })
-    elif option == 1:
-        for card in db:
-            print(card.doc_id, end='. ')
-            cat = card.get('category')
-            que = card.get('q')
-            ans = card.get('a')
-            ease = card.get('count')
-            due_date = card.get('due')
-            print(f'Category: {cat} || Question: {que} || Answer: {ans} || Ease value: {ease} || Due date: {due_date}\n')
-    input('\nPress -enter- to go back to top\n')
+def run():
+    try:
+        card_manager()
+    except KeyboardInterrupt:
+        pass
+
+def card_manager():
+    while True:
+        wipe()
+        print('Card Manager\n\n1. list all\n2. add card\n3. remove card\n4. Return to main menu')
+        print('> ', end='')
+        option = int(input())
+        if option == 4:
+            wipe(quit=True)
+            break
+        elif option == 3:
+            card_id = int(input('card id to delete: '))
+            db.remove(doc_ids=[card_id])
+        elif option == 2:
+            card_category = input('card category: ')
+            card_qtion = input('card question: ')
+            card_answ = input('card asnwer: ')
+            db.insert({'category': card_category,
+                    'q': card_qtion,
+                    'a': card_answ,
+                    'count': 0,
+                    'due': str(datetime.now())
+                    })
+        elif option == 1:
+            for card in db:
+                print(card.doc_id, end='. ')
+                cat = card.get('category')
+                que = card.get('q')
+                ans = card.get('a')
+                ease = card.get('count')
+                due_date = card.get('due')
+                print(f'Category: {cat} || Question: {que} || Answer: {ans} || Ease value: {ease} || Due date: {due_date}\n')
+        input('\nPress -enter- to go back to top\n')
--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import subprocess
 from extra import wipe
-import learner
+import learner, card_manager
 ##### FLASHCARDS ATESTAT INFO 2024 ####
  #### made by CRACIUN IUSTIN-ANDI ####
   ###        functia main         ###
-while True:
-    wipe()
-    print('Welcome to Flashcards by Craciun Iustin Andi.\n')
-    print('1. Enter Learning Mode')
-    print('2. Enter Card Manager Mode')
-    print('3. Change Ease Settings')
-    print('4. Help (explanation of how the application works)')
-    print('5. Quit')
-    option = int(input('> '))
-    if option == 5:
-        wipe(quit=True)
-        break
-    elif option == 4:
-        print("    Flashcards is a learning app that works on the principle of spaced repetition: if you review a piece of knowledge repeatedly, it will enter your long term memory. Each time you review, the time that knowledge remains in your mind increases, which means that eventually you will only see a card every few months, because it (presumably) is already engrained in your brain.\n    Cards: Cards are pairs of questions and answers. When in Learning Mode, you will first only see the question, and you will try to remember what the answer was. After revealing the answer, if you turned out to be wrong, you will let the program know, and it'll show you that card sooner in the future. If you knew the answer, it'll show you that card less in the future.\n    Ease: Ease refers to the algorithm governing the frequency at which you're shown specific cards. In this program, the first 5 values are specified, and all values above that increase exponentially with powers of two.\n    The Card Manager: The Card Manager is the tool with which you can manage your local card database by adding or removing cards and such.")
-        input("\nPress -enter- to return to top")
-        subprocess.run(['python','./main.py'])
-    elif option == 1:
-        learner.run()
-    elif option == 2:
-        subprocess.run(['python','./card_manager.py'])
-    elif option == 3:
-        subprocess.run(['python','./edit_settings.py'])
+
+def main():
+    while True:
+        wipe()
+        print('Welcome to Flashcards by Craciun Iustin Andi.\n')
+        print('1. Enter Learning Mode')
+        print('2. Enter Card Manager Mode')
+        print('3. Change Ease Settings')
+        print('4. Help (explanation of how the application works)')
+        print('5. Quit')
+        option = int(input('> '))
+        if option == 5:
+            wipe(quit=True)
+            break
+        elif option == 4:
+            print("    Flashcards is a learning app that works on the principle of spaced repetition: if you review a piece of knowledge repeatedly, it will enter your long term memory. Each time you review, the time that knowledge remains in your mind increases, which means that eventually you will only see a card every few months, because it (presumably) is already engrained in your brain.\n    Cards: Cards are pairs of questions and answers. When in Learning Mode, you will first only see the question, and you will try to remember what the answer was. After revealing the answer, if you turned out to be wrong, you will let the program know, and it'll show you that card sooner in the future. If you knew the answer, it'll show you that card less in the future.\n    Ease: Ease refers to the algorithm governing the frequency at which you're shown specific cards. In this program, the first 5 values are specified, and all values above that increase exponentially with powers of two.\n    The Card Manager: The Card Manager is the tool with which you can manage your local card database by adding or removing cards and such.")
+            input("\nPress -enter- to return to top")
+            subprocess.run(['python','./main.py'])
+        elif option == 1:
+            learner.run()
+        elif option == 2:
+            card_manager.run()
+        elif option == 3:
+            subprocess.run(['python','./edit_settings.py'])
+
+try:
+    main()
+except KeyboardInterrupt:
+    wipe(quit=True)
